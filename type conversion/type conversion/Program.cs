f5d9a0ed409e8d80c745d9fd9af73990b6cd96a9
Zane-2244 <49199705+Zane-2244@users.noreply.github.com>
2019-04-04 08:59:10 -0700

    Add files via upload

---
+++ type conversion/type conversion/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace type_conversion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                string number = "1234";
+                byte b = Convert.ToByte(number);
+                Console.WriteLine(b);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("the number could not be converted to a byte.");
+
+               
+            }
+            
+        }
+    }
+}
